91e8d2b13a81d6f2c561c16803ddce8a2c72cd55
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-13 23:23:02 +0530

    chore : add login form with validation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,107 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import InputField from "../utils/InputField";
+import { loginUserAsync } from "../features/auth/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 const Login = () => {
-  return <div>login page</div>;
+  const { isError, message, isSuccess } = useSelector((state) => state.auth);
+  const [error, setError] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isError) {
+      setError(message);
+    }
+
+    if (isSuccess) {
+      toast.success(message);
+      navigate("/feed");
+    }
+  }, [message, isError, isSuccess, navigate]);
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setError("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(loginUserAsync(formData));
+  };
+
+  return (
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }} // Full viewport height
+    >
+      <h1 className="text-secondary pb-5">
+        The <span className="text-primary">Social</span> Network
+      </h1>
+
+      <div
+        className="bg-white p-4 shadow"
+        style={{ borderRadius: "15px", maxWidth: "400px", width: "100%" }}
+      >
+        <h2 className="pb-1">Log in</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="row g-3 text-muted pt-1">
+            <div className="col-md-12">
+              <InputField
+                label="Email"
+                type="text"
+                placeholder="you@company.com"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="col-md-12 pb-3">
+              <InputField
+                label="Password"
+                type="password"
+                placeholder="Please enter your password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {error && (
+              <div className="col-md-12 text-danger pt-0 mt-0">
+                <small>{error}</small>
+              </div>
+            )}
+
+            <button
+              className="btn btn-primary w-100"
+              style={{ borderRadius: "10px" }}
+              type="submit"
+              disabled={!!error}
+            >
+              Log In
+            </button>
+
+            <p className="text-decoration-none pt-3 text-center mb-0">
+              Or would you like to{" "}
+              <Link to="/" className="text-decoration-none">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
+
 export default Login;
