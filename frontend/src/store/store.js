64ff497eba74463f1d46112202cf1de482419351
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-15 10:20:59 +0530

    chore : add actions into the store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore, } from "@reduxjs/toolkit"
 import authSlice from "../features/auth/auth"
+import feedSlice from "../features/feed/feedSlice"
 
 export const store = configureStore({
     reducer: {
-        auth: authSlice
+        auth: authSlice,
+        feed: feedSlice
     }
 }) 
