f80384391cf8cd7bfe258435e1aab18ea9ba3d9a
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-15 10:20:10 +0530

    feat : add feedSlice to manage feed page

---
+++ frontend/src/features/feed/feedSlice.js
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchFeedData = createAsyncThunk("feed/fetchFeedData", async (_, { rejectWithValue, getState }) => {
+    const token = localStorage.getItem("token") //acxess the tokem from auth slice
+    console.log(token)
+    try {
+        const response = await axios.get("http://localhost:3000/feed", {
+            headers: {
+                Authorization: `Bearer ${token}`//send token in authorization header
+            }
+        })
+        console.log(response.data)
+        return response.data
+    } catch (error) {
+        console.log(error.response)
+        return rejectWithValue(error.response ? error.response.data : "An unexpected error occured")
+    }
+})
+
+export const feedSlice = createSlice({
+    name: "feed",
+    initialState: {
+        feedData: [{ name: "chethan" }],
+        isLoading: false,
+        error: null
+    },
+    reducers: {
+        clearError: (state) => {
+            state.error = null; // Add a way to clear the error manually
+        },
+    },
+    extraReducers: (builder) => {
+        builder.
+            addCase(fetchFeedData.pending, (state) => {
+                state.isLoading = true;
+                state.error = null
+            })
+            .addCase(fetchFeedData.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.feedData = action.payload;
+                state.error = null
+            })
+            .addCase(fetchFeedData.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            })
+    }
+})
+
+export const { clearError } = feedSlice.actions;
+export default feedSlice.reducer
