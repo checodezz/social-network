115ed48a6f0631c23eaa1e2dafcf13f849f41d2a
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-09-17 00:53:35 +0530

    chore : add conditional routing for protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,43 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
+import Header from "./components/Header";
+import UserProfile from "./pages/UserProfile";
+
+const Layout = ({ children }) => {
+  const location = useLocation();
+
+  const hideHeader =
+    location.pathname === "/" || location.pathname === "/login";
+
+  return (
+    <>
+      {!hideHeader && <Header />}
+      {children}
+    </>
+  );
+};
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/feed" element={<Feed />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile" element={<UserProfile />} />
+          </Routes>
+        </Layout>
       </Router>
     </>
   );
